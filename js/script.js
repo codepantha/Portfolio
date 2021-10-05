64ccb5334255e3ce0db73afc3018aa0af29e6646
codepanther <pezechigozirim@gmail.com>
2021-10-05 22:03:06 +0100

    Display works and the project description page dynamically using javascript

--- js/script.js
+++ js/script.js
@@ -1,85 +1,107 @@
-const menuButton = document.getElementById("menuButton");
-const closeButton = document.getElementById("closeButton");
-const menuCover = document.getElementById("mobileMenuCover");
-const main = document.querySelector(".main");
-const mobileItems = document.querySelectorAll(".mobileMenuItems");
+const menuButton = document.getElementById('menuButton');
+const closeButton = document.getElementById('closeButton');
+const menuCover = document.getElementById('mobileMenuCover');
+const main = document.querySelector('.main');
+const mobileItems = document.querySelectorAll('.mobileMenuItems');
+let modalState = 'closed';
 
-//Work section
-const workSection = document.getElementById("works");
-const arrayOfProyects = [
+// Work section
+const workSection = document.getElementById('works');
+const arrayOfProjects = [
   {
-    image: "./img/smartfacedetector.PNG",
-    title: "My Smart Face Detector App",
-    tech: ["nodejs", "css", "Javascript", "html"],
-    description: "uriwueriuroiweuroeqwiuroweiruweriuweoruiwe",
+    image: './img/smartfacedetector.PNG',
+    title: 'My Smart Face Detector App',
+    tech: ['nodejs', 'css', 'Javascript', 'html'],
+    description: "uriwueriuroiweurLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaoeqwiuroweiruweriuweoruiwe",
   },
   {
-    image: "./img/jagabuy.PNG",
-    title: "Jagabuy Online Shopping Store",
-    tech: ["nodejs", "css", "Javascript", "html"],
-    description: "uriwueriuroiweuroeqwiuroweiruweriuweoruiwe",
+    image: './img/jagabuy.PNG',
+    title: 'Jagabuy Online Shopping Store',
+    tech: ['nodejs', 'css', 'Javascript', 'html'],
+    description: "uriwueriuroiweurLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaoeqwiuroweiruweriuweoruiwe",
   },
   {
-    image: "./img/rephidim.PNG",
-    title: "Rephidim Microfinance Bank",
-    tech: ["nodejs", "css", "Javascript", "html"],
-    description: "uriwueriuroiweuroeqwiuroweiruweriuweoruiwe",
+    image: './img/rephidim.PNG',
+    title: 'Rephidim Microfinance Bank',
+    tech: ['nodejs', 'css', 'Javascript', 'html'],
+    description: "uriwueriuroiweurLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaoeqwiuroweiruweriuweoruiwe",
   },
   {
-    image: "./img/smartfacedetector.PNG",
-    title: "My Smart Face Detector App",
-    tech: ["nodejs", "css", "Javascript", "html"],
-    description: "uriwueriuroiweuroeqwiuroweiruweriuweoruiwe",
+    image: './img/smartfacedetector.PNG',
+    title: 'My Smart Face Detector App',
+    tech: ['nodejs', 'css', 'Javascript', 'html'],
+    description: "uriwueriuroiweurLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaoeqwiuroweiruweriuweoruiwe",
   },
   {
-    image: "./img/jagabuy.PNG",
-    title: "Jagabuy Online Shopping Store",
-    tech: ["nodejs", "css", "Javascript", "html"],
-    description: "uriwueriuroiweuroeqwiuroweiruweriuweoruiwe",
+    image: './img/jagabuy.PNG',
+    title: 'Jagabuy Online Shopping Store',
+    tech: ['nodejs', 'css', 'Javascript', 'html'],
+    description: "uriwueriuroiweurLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaoeqwiuroweiruweriuweoruiwe",
   },
   {
-    image: "./img/rephidim.PNG",
-    title: "Rephidim Microfinance Bank",
-    tech: ["nodejs", "css", "Javascript", "html"],
-    description: "uriwueriuroiweuroeqwiuroweiruweriuweoruiwe",
+    image: './img/rephidim.PNG',
+    title: 'Rephidim Microfinance Bank',
+    tech: ['nodejs', 'css', 'Javascript', 'html'],
+    description: "uriwueriuroiweurLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaoeqwiuroweiruweriuweoruiwe",
   },
-  
+
 ];
 
-for (let x in arrayOfProyects) {
+for (let x = 0; x < arrayOfProjects.length; x += 1) {
   workSection.innerHTML += `<div class="card border">
-  <img src="${arrayOfProyects[x].image}" alt="${arrayOfProyects[x].title}" />
+  <img src="${arrayOfProjects[x].image}" alt="${arrayOfProjects[x].title}" />
   <div class="project-name">
-    <h4>${arrayOfProyects[x].title}</h4>
+    <h4>${arrayOfProjects[x].title}</h4>
   </div>
   <ul class="tech-group">
-  ${arrayOfProyects[x].tech
+  ${arrayOfProjects[x].tech
     .map((tech) => `<li class="tech">${tech}</li>`)
-    .join(" ")}
+    .join(' ')}
   </ul>
-  <button class="btn" onclick="openModal(${x})" type="button">See Project</button>
+  <button class="btn" onclick="toggleModal(${x})" type="button">See Project</button>
 </div>`;
 }
 
-function openModal(position) {
-  
+function toggleModal(position = null) {
+  if (modalState === 'closed') {
+    document.getElementById('modal-title').innerHTML = arrayOfProjects[position].title;
+    document.getElementById('modal-img').src = arrayOfProjects[position].image;
+    const modalTech = document.getElementById('modal-tech');
+
+    for (let i = 0; i < arrayOfProjects[position].tech.length; i += 1) {
+      modalTech.innerHTML += `<li class="tech">${arrayOfProjects[position].tech[i]}</li>`;
+    }
+
+    document.getElementById('modal-desc').innerHTML = arrayOfProjects[position].description;
+    document.querySelector('.projectDetailsModal').style.display = 'block';
+    modalState = 'open';
+  } else {
+    document.querySelector('.projectDetailsModal').style.display = 'none';
+    modalState = 'closed';
+  }
 }
 
+document.querySelector('.close-modal-button').addEventListener('click', () => toggleModal());
+
 function toggleMenu() {
-  menuButton.style.display = "none";
-  menuCover.style.display = "block";
-  main.style.filter = "blur(8px)";
+  menuButton.style.display = 'none';
+  menuCover.style.display = 'block';
+  main.style.filter = 'blur(8px)';
 }
 
 function closeMenu() {
-  menuCover.style.display = "none";
-  menuButton.style.display = "flex";
-  main.style.filter = "blur(0px)";
+  menuCover.style.display = 'none';
+  menuButton.style.display = 'flex';
+  main.style.filter = 'blur(0px)';
 }
 
 mobileItems.forEach((element) => {
-  element.addEventListener("click", closeMenu);
+  element.addEventListener('click', closeMenu);
 });
 
-menuButton.addEventListener("click", toggleMenu);
-closeButton.addEventListener("click", closeMenu);
+menuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', closeMenu);
+menuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', closeMenu);
+menuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', closeMenu);
