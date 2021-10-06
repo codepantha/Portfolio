e05359ceef21e60d6c82ff608c94c9b6cb1132c7
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-10-06 12:19:22 -0500

    Email input validation with JS

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,20 +1,20 @@
-function validateForm(e) {
-  const email = document.querySelector("#email");
-  const emailError = document.querySelector("#emailError");
+function validateForm(form) {
+  console.log("iamreaching")
+  const email = document.getElementById("email");
+  const emailError = document.getElementById("emailError");
 
   console.log(email.value);
 
-  if (email.value !== email.value.toLowerCase()) {
-    e.preventDefault();
+  if (email.value === email.value.toLowerCase()) {
+    console.log('true')
+    return true;
+  } else {
     const errorMessage = "Email should be lowercase.";
     emailError.innerHTML = errorMessage;
     form.email.focus();
     console.log('false')
     return false;
-  } else {
-    console.log('true')
-    return true;
+    
   }
-  return false;
 }
 
