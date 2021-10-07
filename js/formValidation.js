920d12ba031dba1c8e323daa955f3c7d8f6c346d
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-10-07 11:17:04 -0500

    Persist user data using localStorage

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
+const email = document.getElementById('email');
+const nameInput = document.getElementById('name');
+const messageInput = document.getElementById('message');
 function validateForm(form) {
-  const email = document.getElementById('email');
   const emailError = document.getElementById('emailError');
 
   if (email.value === email.value.toLowerCase()) {
@@ -14,3 +16,47 @@ function validateForm(form) {
   email.style.boxShadow = '0 0 2pt 1pt #ff0000';
   return false;
 }
+
+
+ //****Saving oninput fields****//
+let localData = {name: "", email: "", message: ""}
+
+const onChangeInputHandler = (event) => {
+  // console.log(event.target.value)
+  // console.log("input:",event.value)
+  // console.log(event.name)
+
+  switch(event.name){
+    case "name":
+    localData = {...localData, name: event.value}
+    break;
+    case "email":
+      localData = {...localData, email: event.value}
+      break;
+    case "message":
+      localData = {...localData, message: event.value}
+      break;
+
+    default:
+    
+    break;
+  }
+
+  localStorage.setItem("data", JSON.stringify(localData))
+
+}
+
+//***When page reloads ***/
+
+let reloadData = JSON.parse(localStorage.getItem("data"))
+
+if(reloadData){
+  email.value = reloadData.email
+  nameInput.value = reloadData.name
+  messageInput.value = reloadData.message
+}
+
+
+
+
+
